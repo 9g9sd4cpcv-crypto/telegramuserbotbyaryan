c77a948c980ee75ac60994594384900522312b9f
9g9sd4cpcv-crypto <9g9sd4cpcv@privaterelay.appleid.com>
2026-04-16 15:12:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from pyrogram import Client, filters
+from config import *
+from modules import *
+
+app = Client(
+    "userbot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=SESSION
+)
+
+@app.on_message(filters.command("alive", "."))
+async def alive(_, m):
+    await m.reply("🔥 Userbot Alive")
+
+@app.on_message(filters.command("ping", "."))
+async def ping(_, m):
+    await m.reply("🏓 Pong")
+
+# LOAD MODULES
+admin.init(app)
+ranks.init(app)
+activity.init(app)
+filter.init(app)
+profile.init(app)
+music.init(app)
+logger.init(app)
+welcome.init(app)
+
+app.run()
