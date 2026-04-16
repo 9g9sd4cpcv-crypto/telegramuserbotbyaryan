00c2a30ac183bb47eed2f5cf717c8bfc683bd1d6
9g9sd4cpcv-crypto <9g9sd4cpcv@privaterelay.appleid.com>
2026-04-16 15:31:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-SESSION = os.getenv("SESSION")
+def must_get(name):
+    value = os.getenv(name)
+    if not value:
+        raise Exception(f"❌ Missing ENV variable: {name}")
+    return value
 
-MONGO_URL = os.getenv("MONGO_URL")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+API_ID = int(must_get("API_ID"))
+API_HASH = must_get("API_HASH")
+SESSION = must_get("SESSION")
+
+MONGO_URL = must_get("MONGO_URL")
+OWNER_ID = int(must_get("OWNER_ID"))
